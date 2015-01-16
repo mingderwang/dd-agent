0a5b90ecbb0c26845d0500bdc6f13bf0772908b8
Léo Cavaillé <leo@datadoghq.com>
2015-01-16 02:13:42 +0100

    Minor improvements to run tests locally

--- ci/default.rb
+++ ci/default.rb
@@ -9,7 +9,7 @@ namespace :ci do
     task :before_script => ['ci:common:before_script']
 
     task :script => ['ci:common:script'] do
-      sh %(find . -name '*.py'\
+      sh %(find . -name '*.py' -not -path '*venv*' -not -path '*embedded*'\
            | xargs --max-procs=0 -n 1 pylint --rcfile=./.pylintrc)
       Rake::Task['ci:common:run_tests'].invoke('default')
     end
